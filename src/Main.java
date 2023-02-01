2471ec012d9720ba9b91e3f3f56c0f2ea33c4a5d
Engel62 <sr.engel@bk.ru>
2023-02-01 13:01:29 +0300

    1 задание выпоненно

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlad!");
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(3);
+        integerList.add(6);
+        integerList.add(34);
+        integerList.add(46);
+        integerList.add(57);
+        integerList.add(25);
+        integerList.add(19);
+        Stream<Integer> integerStream = integerList.stream();
+        findMinmax(integerStream, Comparator.comparingInt(i -> i), (x, y) ->
+                System.out.println
+                        ("Минимальное значение в стриме " + x + " , " + "максимальное значение в стриме " + y));
     }
+    public static <T> void findMinmax(
+            Stream<? extends  T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> arrayList;
+        T min = null;
+        T max = null;
+        arrayList = stream
+                .sorted(order)
+                .collect(Collectors.toList());
+        if (arrayList.size() != 0) {
+            min = arrayList.get(0);
+            max = arrayList.get(arrayList.size() - 1);
+        }
+        minMaxConsumer.accept(min,max);
+    }
+
 }
